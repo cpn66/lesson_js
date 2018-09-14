5b1106f11f12bd2c44bc60fb7b7a6046c35bca02
cpn@cpn.bs.ugtel <cpn@ugtel.ru>
2018-09-14 17:27:08 +0300

    js

---
+++ webpack-lesson/src/index.js
@@ -0,0 +1,4 @@
+import avg from './some.js';
+//import $ from 'jquery';
+console.log(avg(1,2,3));
+//$('.title').html('Some text1');
---
+++ webpack-lesson/webpack.config.js
@@ -0,0 +1,23 @@
+let path = require('path');
+let conf = {
+  entry:'./src/index.js',
+  output:{
+  	path: path.resolve(__dirname,'./dist'),
+  	filename: 'main.js',
+  	publicPath:'dist/',
+  },
+  devServer:{
+  	overlay:true
+  },
+  module:{
+  	rules:[
+  		{
+  			test:/\.js$/,
+  			//loader:'babel-loader',
+  			// exclude:'/node_modules/'
+  		}
+  	]
+  },
+  devtool:'eval-sourcemap'
+};
+module.exports=conf;
